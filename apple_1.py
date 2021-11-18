675f1914fcb255ccc6ab2ac533151bc41a4ea81e
Joseph-2 <91758003+Joseph-2@users.noreply.github.com>
2021-11-18 13:45:44 -0800

    Update apple_1.py

--- apple_1.py
+++ apple_1.py
@@ -10,7 +10,7 @@ wn.bgpic("background.gif")
 wn.addshape(apple_image) # Make the screen aware of the new file
 
 
-alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+letters = ["A","B","C","D","E","F","G","H","I"]
 
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
@@ -19,35 +19,36 @@ alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
 #TODO Create a function that takes a turtle as its parameter and gives that turtle (apple)
 # a new location on the tree, only if the list of letters is not empty. Associate the 
 # turtle with a new letter selected at random from the list of letters
-def random_location(active_apple):
+def random_location(turtle_index):
   ran_x = rand.randint(-150,150)
   ran_y = rand.randint(-50,150)
-  active_apple.penup()
+  turtles[turtle_index].penup()
   if alphabet:
-    active_apple.goto(ran_x,ran_y)
-  ind = rand.randint(0,len(alphabet))
-  letter = alphabet.pop(ind)
-  active_apple.pendown()
+    turtles[turtle_index].goto(ran_x,ran_y)
+  turtles[turtle_index].pendown()
+  apple_letters = []
+  ind = rand.randint(0,len(letters))
+  apple_letters.append(letters.pop(ind))
 #TODO Create a function that takes a turtle (apple) and its corresponding letter from the letter
 # list and draws that letter on that turtle (apple)
-def write_letter(letter,act_apple):
+def write_letter(letter,turtle_index):
   x = act_apple.xcor()
   y = act_apple.ycor()
   wn.tracer(False)
-  act_apple.penup()
-  act_apple.goto(x-5,y-20)
-  act_apple.pendown()
-  act_apple.color("White")
-  act_apple.write(letter,font=("Arial",25,"bold"))
-  act_apple.penup()
-  act_apple.goto(x,y)
-  act_apple.pendown()
+  turtles[turtle_index].penup()
+  turtles[turtle_index].goto(x-5,y-20)
+  turtles[turtle_index].pendown()
+  turtles[turtle_index].color("White")
+  turtles[turtle_index].write(letter,font=("Arial",25,"bold"))
+  turtles[turtle_index].penup()
+  turtles[turtle_index].goto(x,y)
+  turtles[turtle_index].pendown()
   wn.tracer(True)
 #TODO Create a function that takes a turtle (apple) and its corresponding ltter from the letter
 # list and set that turtle to be shaped by the image file, call the letter drawing function,
 # and update the Screen
-def draw_apple(letter,active_apple):
-  active_apple.shape(apple_image)
+def draw_apple(letter,turtle_index):
+  turtles[turtle_index].shape(apple_image)
   write_letter(letter,active_apple)
   wn.update()
 
@@ -66,21 +67,57 @@ for i in range(0,5):
 #TODO Create a function that takes a letter as its parameter, uses that letter to retrieve the
 # corresponding turtle (apple) and causes both to drop from the tree simultaneously. Once the 
 # apple and letter have dropped, call the apple reseting function.
-def apple_drop(letter):
-  apple.clear()
+def apple_drop(letter_index):
+  turtles[letter_index].clear()
   xcor = apple.xcor()
-  apple.penup()
-  apple.goto(xcor,-175)
-  random_location(apple)
+  turtles[letter_index].penup()
+  turtles[letter_index].goto(xcor,-175)
+  random_location(turtles[letter_index])
 #TODO define a function per letter that you will use in your program. Each function should check
 # to see if the given letter is in the list of letters; if it is, it should drop the corresponding
 # apple.
-def 
+def typed_A():
+  for x in range(0,len(turtles)):
+    if apple_letters[x] == "A":
+      apple_drop[x]
+def typed_B():
+  for x in range(0,len(turtles)):
+    if apple_letters[x] == "B":
+      apple_drop[x]
+def typed_C():
+  for x in range(0,len(turtles)):
+    if apple_letters[x] == "C":
+      apple_drop[x]
+def typed_D():
+  for x in range(0,len(turtles)):
+    if apple_letters[x] == "D":
+      apple_drop[x]
+def typed_E():
+  for x in range(0,len(turtles)):
+    if apple_letters[x] == "E":
+      apple_drop[x]
+def typed_F():
+  for x in range(0,len(turtles)):
+    if apple_letters[x] == "F":
+      apple_drop[x]
+def typed_G():
+  for x in range(0,len(turtles)):
+    if apple_letters[x] == "G":
+      apple_drop[x]
+def typed_H():
+  for x in range(0,len(turtles)):
+    if apple_letters[x] == "H":
+      apple_drop[x]
+def typed_I():
+  for x in range(0,len(turtles)):
+    if apple_letters[x] == "I":
+      apple_drop[x]
 #TODO use the onkeypress method of wn to correlate the functions you defined above with each
 # of the letters that the user might type.
 # onkeypress requires that you name one function that must take
 # no arguments to be called when the specified key is pressed.
-
+def letter_press():
+  
 #-----function calls----
 wn.onkeypress()
 wn.listen()
